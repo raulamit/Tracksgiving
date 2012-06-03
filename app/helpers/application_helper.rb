403f78bef65d13012ff566983455d9e926866ae4
raulamit <amit.c.raul@gmail.com>
2012-06-03 10:30:55 -0700

    tracksgiving

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,3 @@
 module ApplicationHelper
-  def coderay(text)
-  text.gsub(/\<code( lang="(.+?)")?\>(.+?)\<\/code\>/m) do
-    content_tag("notextile", CodeRay.scan($3, $2).div(:css => :class))
-  end
-end
-
+ 
 end
